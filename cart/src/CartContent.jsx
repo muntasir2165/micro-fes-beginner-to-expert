da374915e12a5a8fd1176744a56c820c787638d4
Muntasir Biojid <muntasir2165@hotmail.com>
2022-06-04 18:41:16 -0400

    sharing the jwt

---
+++ cart/src/CartContent.jsx
@@ -0,0 +1,19 @@
+import React, { useState, useEffect } from "react";
+
+import Login from "./Login";
+
+import { jwt } from "./cart";
+
+export default function CartContent() {
+  const [token, setToken] = useState("");
+
+  useEffect(() => {
+    return jwt.subscribe((val) => setToken(val ?? ""), []);
+  });
+  return (
+    <div>
+      <div>JWT: {token}</div>
+      <Login />
+    </div>
+  );
+}
